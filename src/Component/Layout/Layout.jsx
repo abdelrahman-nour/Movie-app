fe9a609e8dd854a3b93cf5a6704a601236773f36
abdelrahman-nour <nourgrdesign@gmail.com>
2026-09-07 17:17:27 +0300

    Edit NameFiles

--- src/Component/Layout/Layout.jsx
+++ src/Component/Layout/Layout.jsx
@@ -1,13 +1,13 @@
 import Navbar from "../Layout/Navbar";
 import Footer from "../Layout/Footer";
-import Search from "../Search/search.jsx";
-import movieCard from "../Cards/movieCard.jsx";
+import Search from "../Search/Search.jsx";
+import MovieCard from "../Cards/MovieCard.jsx";
 export default function Layout() {
   return (
     <>
       <Navbar />
       <Search />
-      <movieCard />
+      <MovieCard />
       <Footer />
     </>
   );
