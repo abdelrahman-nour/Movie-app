84eff7eb01da420acc29c157ce39ffe09525202e
MohamedAboels3oud <mohamed.aboelsoud220@gmail.com>
2026-09-09 21:29:25 +0300

    Add AI Movie Assistant

--- src/Component/Layout/Layout.jsx
+++ src/Component/Layout/Layout.jsx
@@ -30,7 +30,7 @@
 import Navbar from "./Navbar.jsx";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer.jsx";
-
+import AIChat from "../AIChat/AIChat.jsx";
 export default function Layout() {
   return (
     <>
@@ -40,7 +40,7 @@ export default function Layout() {
         <main className="flex-1">
           <Outlet />
         </main>
-
+        <AIChat/>
         <Footer />
       </div>
     </>
